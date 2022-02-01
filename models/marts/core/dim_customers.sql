18fc4752393da376f87b8a363e880b951557f29d
Iyanu Emmanuel <e.iyanuloluwa@gmail.com>
2022-02-01 15:39:11 +0000

    recreated tables and views

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,29 +1,3 @@
--- {{ config(
---     materialized="table"
--- )}}
-
--- with customers as (
-
---     select
---         id as customer_id,
---         first_name,
---         last_name
-
---     from jaffle_shop.customers
-
--- ),
-
--- orders as (
-
---     select
---         id as order_id,
---         user_id as customer_id,
---         order_date,
---         status
-
---     from jaffle_shop.orders
-
--- ),
 
 with customers as (
     select * from {{ ref('stg_customers')}}
